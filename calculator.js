6ddf067f78b7dff4f23b91297780d4978b87d694
capra-irata <113552738+capra-irata@users.noreply.github.com>
2023-01-24 13:20:24 -0800

    Add listeners to number buttons, rename HTML IDs
    On click, append the number button's text to the current display. Rename
    some HTML IDs for better readability. Change % button to backspace
    button.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,24 @@
+initialize();
+
+function initialize() {
+  const numberButtons = document.querySelectorAll(`#number-section .number`);
+  numberButtons.forEach((button) => {
+    button.addEventListener(`click`, appendNumber);
+  });
+}
+
+function appendNumber(e) {
+  const display = getDisplayNode();
+  // If the display reads 0, replace display instead of appending
+  display.textContent === `0`
+    ? display.textContent = e.target.textContent
+    : display.textContent = display.textContent + e.target.textContent;
+}
+
+function getDisplayNode() {
+  return document.querySelector(`#display`);
+}
+
 function add(a, b) {
   return a + b;
 }
@@ -11,11 +32,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return (b === 0) ? `ERR: div by 0` : a / b;
+  return b === 0 ? `ERR: div by 0` : a / b;
 }
 
 function operate(a, b, operator) {
-  switch(operator) {
+  switch (operator) {
     case `+`:
       return add(a, b);
     case `-`:
